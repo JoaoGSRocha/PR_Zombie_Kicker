3ccdd25db239fad273a61cc390627c77e362a34d
Joao Rocha <joaogsrocha@gmail.com>
2015-12-03 04:56:01 +0000

    Added character collider and basic movement.

---
+++ Assets/RobotController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotController : MonoBehaviour
+{
+	public float maxSpeed = 10f;
+	bool facingRight = true;
+
+	bool grounded = false;
+
+	void Start()
+	{}
+	
+	void FixedUpdate()
+	{
+		float move = Input.GetAxis("Horizontal");
+		
+		rigidbody2D.velocity = new Vector2(move* maxSpeed, rigidbody2D.velocity.y);
+		if (move > 0 &&!facingRight)
+		{
+			Flip();
+		}else if(move < 0 && facingRight)
+		{
+			Flip();
+		}
+	}
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+	
+	
+}
