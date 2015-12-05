ba742d1c95edbbffbc2c9c670597879a4d3efcf9
Alexandre Nunes <alexandrenunes1331@gmail.com>
2015-12-05 20:09:34 +0000

    fix spawner bug

--- Assets/spawnEnem.js
+++ Assets/spawnEnem.js
@@ -1,5 +1,11 @@
 ﻿#pragma strict
 var spawnObj: GameObject;
+public var lifes = 6;
+static var toLeft = false;
+var enemy: GameObject;
+private var moveSpeed : Vector2;
+var rb: Rigidbody2D;
+
 function Start()
 {
 	InvokeRepeating("Spawn",  5, 5);
@@ -7,9 +13,18 @@ function Start()
 
 function Spawn()
 {
-	Instantiate(spawnObj, new Vector2(7.58f, -0.49f), Quaternion.identity);
+	if(spawnObj.name=="Spawner_02"){
+		Instantiate(enemy, new Vector2(spawnObj.transform.position.x,spawnObj.transform.position.y), Quaternion.identity);
+		toLeft=true;
+	}else{
+		toLeft=false;
+		Instantiate(enemy, new Vector2(spawnObj.transform.position.x,spawnObj.transform.position.y), Quaternion.identity);
+	}
 }
 
 function Update () {
 
 }
+
+
+	
